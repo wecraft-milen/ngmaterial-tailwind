9fa615184613f5c955135e022e53028f6a09302d
wecraft-milen <milen.ivanov@wecraftmedia.com>
2022-05-17 12:18:39 +0300

    config tailwind to use css variables from :root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 
+import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
